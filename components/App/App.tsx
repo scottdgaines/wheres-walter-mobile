4901ac88672b430582b2c6093c3d9f8cc314dd0a
Scott Gaines <scottdgaines@gmail.com>
2023-05-16 17:24:21 -0600

    Reformat file orgnaization, update file extension in node_modules/expo, and remove asset files

--- components/App/App.tsx
+++ components/App/App.tsx
@@ -1,12 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Where's Walter?</Text>
-      <StatusBar style="auto" />
-    </View>
+   <View>
+    <Text>Hello</Text>
+   </View>
   );
 }
 
