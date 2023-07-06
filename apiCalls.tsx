d726035b8b1bfca9fbbb7ca422a9628325251efc
Scott Gaines <scottdgaines@gmail.com>
2023-07-06 12:26:35 -0600

    Refactor conventional syntax

--- apiCalls.tsx
+++ apiCalls.tsx
@@ -1,10 +1,10 @@
-import cleanData from './utils'
-import { CleanedPet } from './interfaces'
+import cleanData from './utils';
+import { CleanedPet } from './interfaces';
 
 const fetchData = async (): Promise<CleanedPet[]> => {
-    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=q')
-    const data = await response.json()
-    const notices = await cleanData(data.drinks)
+    const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=q');
+    const data = await response.json();
+    const notices = await cleanData(data.drinks);
     return notices
 }
 
